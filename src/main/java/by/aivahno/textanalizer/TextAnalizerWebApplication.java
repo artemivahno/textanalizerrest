70be0eb9fa4beb80dd7e85c9d91c552f6876f3af
ArtemDom <artsiom.ivahno@gmail.com>
2018-08-24 23:27:16 +0300

    join config whith start app

--- src/main/java/by/aivahno/textanalizer/TextAnalizerWebApplication.java
+++ src/main/java/by/aivahno/textanalizer/TextAnalizerWebApplication.java
@@ -2,11 +2,22 @@ package by.aivahno.textanalizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class TextAnalizerWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TextAnalizerWebApplication.class, args);
+	}
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("3000KB");
+		factory.setMaxRequestSize("3000KB");
+		return factory.createMultipartConfig();
 	}
 }
